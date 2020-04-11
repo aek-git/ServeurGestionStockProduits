af0055f874d74e381dd8786a1cc124ff23ca589a
aek-git <a.elkhou@yahoo.fr>
2020-04-11 13:01:25 +0200

    first commit

---
+++ src/test/java/com/bsc/demo/ServeurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
